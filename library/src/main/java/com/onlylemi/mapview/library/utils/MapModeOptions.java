c8b0fdad0cac826e3fce77a107ec938b17156b26
Patrik Nyman <patrik.nyman@virtualstores.se>
2017-08-21 11:02:41 +0200

    Corrected RouteLayer to the new way of doing things

--- library/src/main/java/com/onlylemi/mapview/library/utils/MapModeOptions.java
+++ library/src/main/java/com/onlylemi/mapview/library/utils/MapModeOptions.java
@@ -13,6 +13,10 @@ public class MapModeOptions {
     public Bitmap backgroundImage = null;
     public int backgroundColor = Color.GRAY;
 
+    //RouteLayer options
+    public float routeLineWidth = 4.0f;
+    public int routeLineColor = Color.BLUE;
+
     //Everything with Zoom
     //The map calculates the default zoom or the "middle" which will include the entire map within the view.
     //This padding represents any additional zooming above initial value (Entire map within view zoom)
