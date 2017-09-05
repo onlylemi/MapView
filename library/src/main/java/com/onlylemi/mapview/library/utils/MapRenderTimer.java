c52a4852dd4e764683589e66303232d416b3dd12
Patrik Nyman <patrik.nyman@virtualstores.se>
2017-09-05 10:28:13 +0200

    RenderTimer - Optimization * Removed unused milisecond field from render timer after learning it is less effiecient and potentially incaccurate

--- library/src/main/java/com/onlylemi/mapview/library/utils/MapRenderTimer.java
+++ library/src/main/java/com/onlylemi/mapview/library/utils/MapRenderTimer.java
@@ -7,31 +7,21 @@ package com.onlylemi.mapview.library.utils;
 public class MapRenderTimer {
 
     private long currentTimeNano;
-    private long currentTimeMili;
     private long deltaNano;
-    private long deltaMili;
 
 
     public void start() {
         currentTimeNano = System.nanoTime();
-        currentTimeMili = System.currentTimeMillis();
     }
 
     public void update() {
         deltaNano = System.nanoTime() - currentTimeNano;
-        deltaMili = System.currentTimeMillis() - currentTimeMili;
         currentTimeNano = System.nanoTime();
-        currentTimeMili = System.currentTimeMillis();
     }
 
     public long getFrameTimeNano() {
         return deltaNano;
     }
-
-    public long getFrameTimeMili() {
-        return deltaMili;
-    }
-
 
     //Do we need stop?
 }
