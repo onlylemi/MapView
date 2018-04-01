78764255e861a068e49d8b57806c8caee7aa5421
Patrik Nyman <patrik911217@gmail.com>
2018-04-01 12:27:29 +0200

    Extraced the base functionality of the contain mode into a base class, this gives the basic functionaltiy of transpiling from one matrix state into another and can be shared among all my contain camera modes

--- library/src/main/java/com/onlylemi/mapview/library/camera/ContainUserMode.java
+++ library/src/main/java/com/onlylemi/mapview/library/camera/ContainUserMode.java
@@ -2,112 +2,64 @@ package com.onlylemi.mapview.library.camera;
 
 import android.graphics.Matrix;
 import android.graphics.PointF;
-import android.util.Log;
+import android.support.annotation.NonNull;
 
 import com.onlylemi.mapview.library.MapViewCamera;
 import com.onlylemi.mapview.library.graphics.implementation.LocationUser;
 import com.onlylemi.mapview.library.utils.MapMath;
-import com.onlylemi.mapview.library.utils.MapUtils;
 
 /**
  * Created by patnym on 27/12/2017.
  */
 
-public class ContainUserMode extends BaseMode {
+public class ContainUserMode extends BaseContainMode {
     private static final String TAG = "ContainUserMode";
 
     private LocationUser user;
 
-    private float defaultContainZoom;
-
-    private long maxTimeToReturnNano = 500000000l;
-    private long defaultTimeToReturn = 50000000l; //Default to spend 0.05 second to translate
-
-    private PointF targetPos;
-    private PointF translateDirection;
-
-    private float translateDistance;
-    private float zoomDistance;
-
-    private float zoomSpeed;
-    private float translateSpeed;
-
-    //Cached values to prevent GC
-    private float savedZoom;
-    private PointF savedPosition;
-    private long timeSpentReturning;
-
-    private PointF lastUserPosition = new PointF();
+    private PointF lastUserPosition;
 
     public ContainUserMode(MapViewCamera camera, LocationUser user) {
         super(camera);
         this.user = user;
-        this.defaultContainZoom = camera.getDefaultContainUserZoom();
+        lastUserPosition = new PointF();
     }
 
     @Override
     public void onViewChanged() {
-        defaultContainZoom = camera.getDefaultContainUserZoom();
     }
 
     @Override
     public void onStart() {
-        initTranslation();
-        initZooming();
+        targetedZoom = camera.getDefaultContainUserZoom();
+        targetedPosition = calculateTargetPosition(targetedZoom);
         timeSpentReturning = maxTimeToReturnNano;
         lastUserPosition.set(user.getPosition());
+        super.onStart();
     }
 
     @Override
     public Matrix update(Matrix worldMatrix, long deltaTimeNano) {
         if (lastUserPosition.x != user.getPosition().x || lastUserPosition.y != user.getPosition().y) {
             //If we got time left on returning this means the user moved during a init step
-            initTranslation(timeSpentReturning > 0 ? timeSpentReturning : defaultTimeToReturn);
-        } else if(translateDistance < 0 && zoomDistance < 0) {
+            targetedPosition = calculateTargetPosition(targetedZoom);
+            initTranslation(targetedPosition,
+                    timeSpentReturning > 0 ? timeSpentReturning : defaultTimeToReturn);
+        } else if(translateDistance <= 0 && zoomDistance <= 0) {
             //If both distances have crossed over we just create our view matrix and return
             return createViewMatrix(worldMatrix);
         }
-
-        translateDistance -= Math.abs(translateSpeed) * deltaTimeNano;
-        zoomDistance -= Math.abs(zoomSpeed) * deltaTimeNano;
-
-        savedPosition = MapUtils.positionFromMatrix(worldMatrix);
-        if(translateDistance > 0) {
-            savedPosition.x += deltaTimeNano * translateSpeed * translateDirection.x;
-            savedPosition.y += deltaTimeNano * translateSpeed * translateDirection.y;
-            timeSpentReturning -= deltaTimeNano;
-        } else {
-            savedPosition.x = targetPos.x;
-            savedPosition.y = targetPos.y;
-        }
-
-        savedZoom = camera.getCurrentZoom();
-        if(zoomDistance > 0) {
-            savedZoom += deltaTimeNano * zoomSpeed;
-            camera.setCurrentZoom(savedZoom);
-        } else {
-            savedZoom = defaultContainZoom;
-            camera.setCurrentZoom(defaultContainZoom);
-        }
         lastUserPosition.set(user.getPosition());
-
-        return createViewMatrix(worldMatrix);
-    }
-
-    private Matrix createViewMatrix(Matrix m) {
-        float[] ma = { savedZoom, 0, savedPosition.x,
-                0, savedZoom, savedPosition.y,
-                0, 0, 1};
-        m.setValues(ma);
-        return m;
+        return super.update(worldMatrix, deltaTimeNano);
     }
 
     //This function calculates where we wanna end up
-    private PointF calculateTargetPosition() {
+    @NonNull
+    private PointF calculateTargetPosition(float targetZoom) {
         PointF userPositionInMiddleInViewSpace = new PointF((camera.getViewWidth() / 2) - user.getPosition().x,
                 (camera.getViewHeight() / 2) - user.getPosition().y);
         Matrix m = new Matrix();
-        m.setScale(defaultContainZoom, defaultContainZoom, camera.getViewWidth() / 2, camera.getViewHeight() / 2);
+        m.setScale(targetZoom, targetZoom, camera.getViewWidth() / 2, camera.getViewHeight() / 2);
         PointF topLeftCameraCorner = MapMath.transformPoint(m, userPositionInMiddleInViewSpace);
 
         float targetX = topLeftCameraCorner.x;
@@ -115,36 +67,17 @@ public class ContainUserMode extends BaseMode {
 
         if(topLeftCameraCorner.x > 0) {
             targetX = 0;
-        } else if(topLeftCameraCorner.x + (camera.getMapWidth() * defaultContainZoom) < camera.getViewWidth()) {
-            targetX += camera.getViewWidth() - (topLeftCameraCorner.x + (camera.getMapWidth() * defaultContainZoom));
+        } else if(topLeftCameraCorner.x + (camera.getMapWidth() * targetZoom) < camera.getViewWidth()) {
+            targetX += camera.getViewWidth() - (topLeftCameraCorner.x + (camera.getMapWidth() * targetZoom));
         }
 
         if(topLeftCameraCorner.y > 0) {
             targetY = 0;
-        } else if(topLeftCameraCorner.y + (camera.getMapHeight() * defaultContainZoom) < camera.getViewHeight()) {
-            targetY += camera.getViewHeight() - (topLeftCameraCorner.y + (camera.getMapHeight() * defaultContainZoom));
+        } else if(topLeftCameraCorner.y + (camera.getMapHeight() * targetZoom) < camera.getViewHeight()) {
+            targetY += camera.getViewHeight() - (topLeftCameraCorner.y + (camera.getMapHeight() * targetZoom));
         }
 
         return new PointF(targetX, targetY);
-    }
-
-    private void initTranslation() {
-        initTranslation(maxTimeToReturnNano);
-    }
-
-    private void initTranslation(long timeToReturnNano) {
-        targetPos = calculateTargetPosition();
-        savedPosition = MapUtils.positionFromMatrix(camera.getWorldMatrix());
-        translateDirection = MapMath.pointSubtract(targetPos, savedPosition);
-        translateDistance = translateDirection.length();
-        translateDirection = MapMath.normalize(translateDirection);
-        translateSpeed = translateDistance / timeToReturnNano;
-    }
-
-    private void initZooming() {
-        zoomDistance = defaultContainZoom - camera.getCurrentZoom();
-        zoomSpeed = zoomDistance / maxTimeToReturnNano;
-        zoomDistance = Math.abs(zoomDistance);
     }
 
     @Override
